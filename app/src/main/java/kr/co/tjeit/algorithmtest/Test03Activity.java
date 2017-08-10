382384e17615a23243a45a922e7b15a543b35500
cho881020 <cho881020@gmail.com>
2017-08-10 15:00:46 +0900

    구구단 2~9단 작동 완료.

--- app/src/main/java/kr/co/tjeit/algorithmtest/Test03Activity.java
+++ app/src/main/java/kr/co/tjeit/algorithmtest/Test03Activity.java
@@ -21,6 +21,13 @@ public class Test03Activity extends AppCompatActivity {
     List<GuguData> guguDatas = new ArrayList<>();
     GuguAdapter mAdapter;
     private Button btn03;
+    private Button btn04;
+    private Button btn05;
+    private Button btn06;
+    private Button btn07;
+    private Button btn08;
+    private Button btn09;
+    private Button btnTotal;
 
 
     @Override
@@ -42,12 +49,34 @@ public class Test03Activity extends AppCompatActivity {
         View.OnClickListener guguListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                printGuguDan(??);
+
+                int dan = Integer.parseInt(v.getTag().toString());
+                // Integer 클래스 : Wrapper 클래스
+                // int 를 감싼다. => int : 기본형 변수.
+                // 그외의 대문자로 시작하는 클래스 : 참조형 변수.
+                // Ex. 참조형 변수 : String => 클래스 (변수, 메쏘드)
+                // int -> 클래스화 : Integer
+                // double -> 클래스화 : Double
+
+                // parse -> 분쇄하다. => 개발 : 분석하다
+                // 날아온 자료를 분석해서 다르게 활용하고자 할때 : parse
+
+
+                printGuguDan(dan);
+                // 클릭된 버튼에 달려있는 Tag를 기반으로
+                // 몇단을 출력해야 하는지 판단하여 실행 메쏘드.
+
             }
         };
 
         btn02.setOnClickListener(guguListener);
         btn03.setOnClickListener(guguListener);
+        btn04.setOnClickListener(guguListener);
+        btn05.setOnClickListener(guguListener);
+        btn06.setOnClickListener(guguListener);
+        btn07.setOnClickListener(guguListener);
+        btn08.setOnClickListener(guguListener);
+        btn09.setOnClickListener(guguListener);
 
 
     }
@@ -73,6 +102,13 @@ public class Test03Activity extends AppCompatActivity {
     private void bindViews() {
 
         this.guguListView = (ListView) findViewById(R.id.guguListView);
+        this.btnTotal = (Button) findViewById(R.id.btnTotal);
+        this.btn09 = (Button) findViewById(R.id.btn09);
+        this.btn08 = (Button) findViewById(R.id.btn08);
+        this.btn07 = (Button) findViewById(R.id.btn07);
+        this.btn06 = (Button) findViewById(R.id.btn06);
+        this.btn05 = (Button) findViewById(R.id.btn05);
+        this.btn04 = (Button) findViewById(R.id.btn04);
         this.btn03 = (Button) findViewById(R.id.btn03);
         this.btn02 = (Button) findViewById(R.id.btn02);
     }
